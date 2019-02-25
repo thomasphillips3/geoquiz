3634356ab28bd2a8ef6c698a8c6948b1c8061716
Thomas Phillips <tphillips@snap.com>
2019-02-25 13:52:39 -0800

    creates the Question model

---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,3 @@
+package com.example.geoquiz
+
+data class Question (val textResId: Int, val answer: Boolean)
--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -1,5 +1,6 @@
 package com.example.geoquiz
 
+import android.app.ProgressDialog.show
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
