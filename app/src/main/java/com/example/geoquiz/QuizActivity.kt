9b90875f6ed85968a4307300cd111966d644d101
Thomas Phillips <tphillips@snap.com>
2019-02-25 12:14:49 -0800

    implements button functionality

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -2,11 +2,36 @@ package com.example.geoquiz
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 }
