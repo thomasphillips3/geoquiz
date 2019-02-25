4342ee1f5823603a81b47ec64b75f6bf5a99a1dd
Thomas Phillips <tphillips@snap.com>
2019-02-25 14:32:00 -0800

    checks to see if the answer is correct when user responds

--- app/src/main/java/com/example/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/example/geoquiz/QuizActivity.kt
@@ -35,19 +35,11 @@ class QuizActivity : AppCompatActivity() {
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.correct_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(true)
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(
-                this,
-                R.string.incorrect_toast,
-                Toast.LENGTH_SHORT)
-                .show()
+            checkAnswer(false)
         }
 
         nextButton.setOnClickListener {
@@ -55,9 +47,23 @@ class QuizActivity : AppCompatActivity() {
             updateQuestion()
         }
     }
-    
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this,
+            messageResId,
+            Toast.LENGTH_SHORT).show()
     }
 }
